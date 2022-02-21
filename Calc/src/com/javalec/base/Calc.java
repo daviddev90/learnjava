c6a7eaeb2be4efe81a80475dbb9f63258d94df6a
TJ <tj@TJui-iMac-7.local>
2022-02-21 18:02:06 +0900

    220221 commit

---
+++ Calc/src/com/javalec/base/Calc.java
@@ -0,0 +1,39 @@
+package com.javalec.base;
+
+public class Calc {
+
+	//main > ctrl + space > enter
+	public static void main(String[] args) {
+		
+		//변수를 이용한 사칙연산
+		int intNum1 = 100;
+		int intNum2 = 20;
+		
+		System.out.println(intNum1 + intNum2); // 100 + 20
+		System.out.println(intNum1 - intNum2); // 100 - 20
+		
+		intNum1 = 10;
+		
+		System.out.println(intNum1 * intNum2); // 10 * 20
+		System.out.println(intNum1 / intNum2); // 10 / 20
+		
+		
+		// 문자와 계산값을 같이 출력하
+		System.out.println("덧셈 :" + (intNum1 + intNum2)); // 덧셈: 30
+		
+		System.out.println("덧셈 : " + intNum1 + " + " + intNum2 + " = " + (intNum1 + intNum2));
+		System.out.println("뺄셈 : " + intNum2 + " - " + intNum1 + " = " + (intNum2 - intNum1));
+		System.out.println("곱셈 : " + intNum1 + " * " + intNum2 + " = " + (intNum1 * intNum2));
+		System.out.println("나눗셈 : " + intNum2 + " / " + intNum1 + " = " + (intNum2 / intNum1));
+		
+		
+		// 상수를 이용한 사칙연산
+		System.out.println(20+10); //30
+		System.out.println(20-10); //10
+		System.out.println(10*20); //200
+		System.out.println(20/10); //2
+		System.out.println(10/20); //0
+	}
+
+}
+
---
+++ Hello/src/com/javalec/base/Hello.java
@@ -0,0 +1,13 @@
+package com.javalec.base;
+
+//안녕하세요.
+//이 프로젝트는 기본개념을 설명하기 위한 프로젝트입니다.
+
+public class Hello { //Class 이름
+
+	public static void main(String[] args) { //Method 이름
+		// TODO Auto-generated method stub
+		System.out.println("Dongwon");
+	}
+
+}
